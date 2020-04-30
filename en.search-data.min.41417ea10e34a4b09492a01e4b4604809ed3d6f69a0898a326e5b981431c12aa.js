605b0af9663f8665fbf1dcbf546a73d032db412d
maxrodrigo <maxrodrigo@users.noreply.github.com>
2020-04-30 07:13:54 +0000

    deploy: aac0f3ffdc494edd84ab02a4ed93f83f41b67b3a

---
+++ en.search-data.min.41417ea10e34a4b09492a01e4b4604809ed3d6f69a0898a326e5b981431c12aa.js
@@ -0,0 +1,1 @@
+'use strict';(function(){const indexCfg={cache:true};indexCfg.doc={id:'id',field:['title','content'],store:['title','href'],};const index=FlexSearch.create('balance',indexCfg);window.bookSearchIndex=index;index.add({'id':0,'href':'/notebook/docs/enumeration/','title':"Enumeration",'content':""});index.add({'id':1,'href':'/notebook/docs/','title':"Docs",'content':""});})();
---
+++ en.search.min.6006ad59303a34e48d02b3ecbb431ef86d545ea42e76476040ad1d818a0495b2.js
@@ -0,0 +1,10 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
+input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
+const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
+input.focus();event.preventDefault();}
+function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/notebook/flexsearch.min.js');loadScript('/notebook/en.search-data.min.41417ea10e34a4b09492a01e4b4604809ed3d6f69a0898a326e5b981431c12aa.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
